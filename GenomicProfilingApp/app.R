3fb5f3d2335f0040c330e557311d170bcda78451
Zack Swales <s2274585@bifx-core2.bio.ed.ac.uk>
2025-02-24 19:04:13 +0000

    Couple of edits to help texts. Visualisation panel now has tabs for heatmaps and average profile plot. Average profile tab has sidebar with limited customisation options (needs improvement). Server code structured better with commenting. Average profile plot server code constructed alongside the download handlers for the average profile plot making both .png and .pdf

--- GenomicProfilingApp/app.R
+++ GenomicProfilingApp/app.R
@@ -11,7 +11,7 @@ ui <- page_navbar(
   # Panel for home page with directions for use of application
   nav_panel(title = "Home",
             card(
-              "Interactive genomic profiling tool to probe internal genomic features. Begin by navigating to the data processing page for file uploading before moving to the visualisations page to generate outputs"
+              "Interactive genomic profiling tool to probe internal genomic features. Begin by navigating to the data processing page for file uploading and matrix computation before moving to the visualisations page to generate outputs"
             )),
   # Panel for data processing and matrix formation
   nav_panel(title = "Data Processing",
@@ -19,7 +19,7 @@ ui <- page_navbar(
               sidebar = sidebar(
                 open = TRUE,
                 width = 325,
-                title = "File Upload",
+                title = "Matrix Generation",
                 fileInput(inputId = "Region1",
                           label = "Upload a region file (.bed/.gtf)",
                           accept = c(".bed", ".gtf"),
@@ -71,18 +71,20 @@ ui <- page_navbar(
   ),
   # Panel for visualisations
   nav_panel(title = "Visualisations",
+            navset_tab(
+              nav_panel("Heatmap",
             layout_sidebar(
               sidebar = sidebar(
                 open = TRUE, 
-                title = "Visualisation Customisation",
+                title = "Heatmap Customisation",
                 radioButtons(
-                  inputId = "col_fun",
+                  inputId = "heatmap_col_fun",
                   label = "Select colour scheme:",
                   choices = list("White to red" = 1, "Blue to red" = 2, "Red scale" = 3),
                   selected = 1
                 ),
                 sliderInput(
-                  inputId = "quantiles",
+                  inputId = "heatmapquantiles",
                   label = "Set quantile range:",
                   min = 0,
                   max = 0.99,
@@ -100,15 +102,15 @@ ui <- page_navbar(
                   value = FALSE
                 ),
                 actionButton(
-                  inputId = "plotbutton",
+                  inputId = "heatmapplotbutton",
                   label = "Plot Output"),
                 helpText("Output plotting will take a few seconds"),
                 helpText("Use download buttons after clicking Plot Output"),
                 downloadButton(
-                  outputId = "downloadpng",
+                  outputId = "heatmapdownloadpng",
                   label = "Download as .png"),
                 downloadButton(
-                  outputId = "downloadpdf",
+                  outputId = "heatmapdownloadpdf",
                   label = "Download as .pdf"
                 )
               ),
@@ -120,11 +122,54 @@ ui <- page_navbar(
                 )
               )
             )
+  ),
+  nav_panel("Average Profile Plot",
+            layout_sidebar(
+              sidebar = sidebar(
+                open = TRUE, 
+                title = "Average Profile Plot Customisation",
+                textInput(
+                  inputId = "plottitle",
+                  label = "Enter plot title:",
+                  value = "Average profile plot"
+                ),
+                sliderInput(
+                  inputId = "averageprofilequantiles",
+                  label = "Set quantile range:",
+                  min = 0,
+                  max = 1,
+                  value = c(0,1)
+                ),
+                actionButton(
+                  inputId = "averageprofileplotbutton",
+                  label = "Plot Output"),
+                helpText("Output plotting will take a few seconds"),
+                helpText("Use download buttons after clicking Plot Output"),
+                downloadButton(
+                  outputId = "averageprofiledownloadpng",
+                  label = "Download as .png"),
+                downloadButton(
+                  outputId = "averageprofiledownloadpdf",
+                  label = "Download as .pdf"
+                )
+              ),
+              card(
+                full_screen = TRUE,
+                card_header("Average Profile Plot"),
+                card_body(
+                  plotOutput("averageprofileplot")
+                )
+              )
+            )
   )
-)
+  )
+))
 
 # Define server logic required to compute matrices and plot outputs
 server <- function(input, output) {
+  
+  ## Data processing
+  
   output$regionfile1_name <- renderText({
     if(!is.null(input$Region1)) {
       paste(input$Region1$name)
@@ -204,33 +249,32 @@ server <- function(input, output) {
     paste(mat_names, collapse = ", ")
   })
   
-  # Creating reactive objects for the visualisation customisation options
+  ### Heatmaps
+  
+  # Creating reactive objects for the heatmaps customisation options
   
   show_row_names_reactive <- reactive({
     input$showrownames
   })
   
-  col_fun_reactive <- reactive({
-    switch(input$col_fun,
+  heatmap_col_fun_reactive <- reactive({
+    switch(input$heatmap_col_fun,
            "1" = "red",
            "2" = "bl2rd",
            "3" = "red0")
   })
   
-  min_quantile_reactive <- reactive({
-    input$quantiles[1]
+  heatmap_min_quantile_reactive <- reactive({
+    input$heatmapquantiles[1]
   })
   
-  max_quantile_reactive <- reactive({
-    input$quantiles[2]
+  heatmap_max_quantile_reactive <- reactive({
+    input$heatmapquantiles[2]
   })
   
   max_ylim_reactive <- reactive({
     input$maxylim
   })
-  
-
-  # Defining the windows and axis labels
   
   wins_reactive <- reactive({
     req(flank_reactive())
@@ -255,24 +299,26 @@ server <- function(input, output) {
     draw(combined_hm, merge_legend = TRUE)
   })
   
-  hml <- eventReactive(input$plotbutton, {
+  hml <- eventReactive(input$heatmapplotbutton, {
     req(matl())
-    hml <- hmList(matl = matl(), wins = wins_reactive(), col_fun = col_fun_reactive(), axis_labels = axis_labels_reactive(), show_row_names = show_row_names_reactive(), min_quantile = min_quantile_reactive(), max_quantile = max_quantile_reactive(), ylim = c(0, max_ylim_reactive()))
+    hml <- hmList(matl = matl(), wins = wins_reactive(), col_fun = heatmap_col_fun_reactive(), axis_labels = axis_labels_reactive(), show_row_names = show_row_names_reactive(), min_quantile = heatmap_min_quantile_reactive(), max_quantile = heatmap_max_quantile_reactive(), ylim = c(0, max_ylim_reactive()))
     return(hml)
   })
-  
-  output$downloadpng <- downloadHandler(
+ 
+  ## Download options for the heatmaps
+   
+  output$heatmapdownloadpng <- downloadHandler(
     filename = function() { paste("heatmap_", Sys.Date(), ".png", sep="") },
     content = function(file) {
       png(file, width = 1350, height = 900, res = 150)  # Adjust quality
       hml <- hmList(
         matl = matl(), 
         wins = wins_reactive(), 
-        col_fun = col_fun_reactive(), 
+        col_fun = heatmap_col_fun_reactive(), 
         axis_labels = axis_labels_reactive(), 
         show_row_names = show_row_names_reactive(), 
-        min_quantile = min_quantile_reactive(), 
-        max_quantile = max_quantile_reactive(), 
+        min_quantile = heatmap_min_quantile_reactive(), 
+        max_quantile = heatmap_max_quantile_reactive(), 
         ylim = c(0, max_ylim_reactive())
       )
       
@@ -283,24 +329,84 @@ server <- function(input, output) {
     }
   )
   
-  output$downloadpdf <- downloadHandler(
+  output$heatmapdownloadpdf <- downloadHandler(
     filename = function() { paste("heatmap_", Sys.Date(), ".pdf", sep="") },
     content = function(file) {
       pdf(file, width = 13.5, height = 9)  # Adjust quality
       hml <- hmList(
         matl = matl(), 
         wins = wins_reactive(), 
-        col_fun = col_fun_reactive(), 
+        col_fun = heatmap_col_fun_reactive(), 
         axis_labels = axis_labels_reactive(), 
         show_row_names = show_row_names_reactive(), 
-        min_quantile = min_quantile_reactive(), 
-        max_quantile = max_quantile_reactive(), 
+        min_quantile = heatmap_min_quantile_reactive(), 
+        max_quantile = heatmap_max_quantile_reactive(), 
         ylim = c(0, max_ylim_reactive())
       )
       
       req(length(hml) > 0)
       combined_hm <- Reduce(`+`, hml)
       draw(combined_hm, merge_legend = TRUE)
+      dev.off()  # Close the pdf file
+    }
+  )
+  
+  ## Average profile plot
+  
+  ## Reactive customisation options for the average profile plots
+  
+  title_reactive <- reactive({
+    input$plottitle
+  })
+  
+  averageprofile_min_quantile_reactive <- reactive({
+    input$averageprofilequantiles[1]
+  })
+  
+  averageprofile_max_quantile_reactive <- reactive({
+    input$averageprofilequantiles[2]
+  })
+  
+  pal = c(RColorBrewer::brewer.pal(n = 9,name = "Set1"))
+  pal2 = c(wes_palette("Darjeeling2")[2],wes_palette("Zissou1")[1],wes_palette("Darjeeling1")[4],wes_palette("Darjeeling1")[3])
+  colmap = c(pal[c(1:5,7:9)])
+
+  
+  
+  # Creating the average profile plot
+  
+  output$averageprofileplot <- renderPlot({
+    req(average_profile())
+    average_profile()
+  })
+  
+  
+  average_profile <- eventReactive(input$averageprofileplotbutton, {
+    req(matl())
+    average_profile <- mplot(matl = matl(), colmap = colmap, title = title_reactive(), min_quantile = averageprofile_min_quantile_reactive(), max_quantile = averageprofile_max_quantile_reactive())
+    return(average_profile)
+  })
+  
+  ## Downloading average profile plots
+  
+  output$averageprofiledownloadpng <- downloadHandler(
+    filename = function() { paste("averageprofileplot_", Sys.Date(), ".png", sep="") },
+    content = function(file) {
+      png(file, width = 1350, height = 900, res = 150)  # Adjust quality
+      average_profile <- mplot(matl = matl(), colmap = colmap, title = title_reactive())
+      
+      print(average_profile)
+      dev.off()  # Close the pdf file
+    }
+  )
+  
+  output$averageprofiledownloadpdf <- downloadHandler(
+    filename = function() { paste("averageprofileplot_", Sys.Date(), ".pdf", sep="") },
+    content = function(file) {
+      pdf(file, width = 13.5, height = 9)  # Adjust quality
+      average_profile <- mplot(matl = matl(), colmap = colmap, title_reactive())
+      
+      print(average_profile)
       dev.off()  # Close the pdf file
     }
   )
